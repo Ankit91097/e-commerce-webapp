a76412906f272ddfee4af1875ea83bff86ba4da6
Ankit91097 <ankitniranjan331@gmail.com>
2024-04-17 22:46:10 +0530

    product add in cart

--- routes/index.js
+++ routes/index.js
@@ -1,156 +1,157 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 const userModel = require('./users')
-const productModel = require('./product')
-const cartModel = require('./cart')
-const cartProductModel = require('./cartProduct')
+const localStrategy = require('passport-local');
+const passport = require('passport');
+const upload=require('./multer')
+const productModel=require('./product')
+const cartModel=require('./cart')
+const cartProductModel=require('./cartproduct')
+passport.use(new localStrategy(userModel.authenticate()));
 
-var users = require('./users')
-var passport = require('passport')
-var localStrategy = require('passport-local')
-passport.use(new localStrategy(users.authenticate()))
-const upload = require('./multer')
 
 /* GET home page. */
-router.get('/', isloggedIn, async function (req, res, next) {
-
-  const allProducts = await productModel.find()
-
-  res.render('index', { title: 'Express', allProducts });
+router.get('/',isLoggedIn,async function (req, res, next) {
+  const allProducts=await productModel.find()
+  res.render('index.ejs',{allProducts});
 });
-
-
 router.get('/register', function (req, res) {
   res.render('register', { title: 'Register' });
 })
-
-
-router.post('/register', function (req, res) {
-
-  var userData = new userModel({
-    username: req.body.username,
-    accountType: req.body.isSeller === 'on' ? "seller" : 'buyer'
-  })
-  userModel
-    .register(userData, req.body.password)
-    .then(function (registeredUser) {
-      passport.authenticate('local')(req, res, function () {
-        console.log('registered user', registeredUser)
-        if (registeredUser.accountType === 'seller') {
-          res.redirect("/createProduct")
-          return
-        }
-        res.redirect('/');
-      })
-    })
-});
-
-function isloggedIn(req, res, next) {
-  if (req.isAuthenticated()) return next();
-  else res.redirect('/login');
-}
-
-function isSeller(req, res, next) {
-  if (req.user.accountType === 'seller') return next()
-  else res.redirect('/')
-}
-
 
 router.get('/login', function (req, res) {
   res.render('login', { title: 'Login' });
 })
 
 
-router.post('/login',passport.authenticate('local',{
-    failureRedirect: '/login',
-  }),
-  (req, res, next) => {
-    if (req.user.accountType == 'seller') {
-      res.redirect('/createProduct')
-    }
-    else {
-      res.redirect('/')
-    }
-  }
-);
-
-
-router.get('/logout', (req, res, next) => {
-  if (req.isAuthenticated())
-    req.logout((err) => {
-      if (err) res.send(err);
-      else res.redirect('/');
-    });
-  else {
-    res.redirect('/');
-  }
-});
-
-router.get('/createProduct', isloggedIn, isSeller, function (req, res) {
+router.get('/createproduct',isLoggedIn,isSeller, function (req, res) {
   res.render('createProduct', { title: 'Create Product' });
 })
 
+router.post('/register', function (req, res, next) {
+  var userData = new userModel({
+    username: req.body.username,
+    email: req.body.email,
+    accountType:req.body.isSeller == "on"?"seller":"buyer",
+  });
+  userModel.register(userData,req.body.password)
+  .then(function(registereduser){
+    passport.authenticate("local")(req,res,function(){
+      if(registereduser.accountType==="seller"){
+        res.redirect("/createproduct")
+        return
+      }
+      else{
+        res.redirect("/")
 
-router.post('/createProduct', isloggedIn, isSeller, upload.array('image'), async (req, res, next) => {
-
-  const newProduct = await productModel.create({
-    name: req.body.name,
-    price: Number(req.body.price),
-    description: req.body.description,
-    user: req.user._id,
-    image: req.files.map(file => {
-      return "/upload/" + file.filename
+      }
     })
   })
+});
 
-  res.redirect('/')
+router.post('/login',passport.authenticate("local",{
+  failureRedirect:"/login"
+}) ,function (req, res, next) {
+  if(req.user.accountType=='seller'){
+    res.redirect("/createproduct");
+  }else{
+    res.redirect("/")
+  }
+});
 
-})
-
-router.get('/cart', (req, res, next) => {
-  res.render('cart')
-})
-router.get('/addToCart/:productId',async (req, res, next) => {
- const productId = req.params.productId;
-
- let  userCart = await cartModel.findOne({
-  user:req.user._id
- })
-
- if(!userCart){
-  userCart = await cartModel.create({
-    user:req.user._id
+router.post('/createproduct',isLoggedIn,isSeller,upload.array("image"),async function (req, res) {
+  const newproduct=await productModel.create({
+    name:req.body.name,
+    price:Number(req.body.price),
+    description:req.body.description,
+    user:req.user._id,
+    image:req.files.map(function(file){
+      return "/upload/"+file.filename
+    })
   })
- }
- let newCartProduct = await cartProductModel.findOne({
-  product:productId,  
-  _id:{$in:userCart.products}
- })
+  res.redirect("/")
+  
+})
 
+router.get('/logout', function(req, res, next){
+  req.logout(function(err) {
+    if (err) { return next(err); }
+    res.redirect('/login');
+  });
+});
 
-if(newCartProduct){
- newCartProduct.quantity = newCartProduct.quantity + 1
- await newCartProduct.save()
+function isLoggedIn(req,res,next){
+  if(req.isAuthenticated()){
+    return next();
+  }
+  res.redirect("/login")
+} 
+function isSeller(req,res,next){
+  if(req.user.accountType==="seller"){
+    return next()
+  }
+  else{
+    res.redirect("/")
+  }
 }
-else{
-  newCartProduct = await cartProductModel.create({
-  product:productId,
-  quantity:1,
+
+router.get('/cart', isLoggedIn,async function (req, res) {
+  const userCart= await cartModel.findOne({
+    user:req.user._id,
+  }).populate('product').populate({
+    path:"product",
+    populate:"product"
+  })
+  let totalPrice=0
+
+  userCart.product.forEach(function(cartproduct){
+    totalPrice+=cartproduct.product.price*(cartproduct.quantity == 0 ? 1 : cartproduct.quantity)
+  })
+  console.log(totalPrice)
+
+  res.render('cart',{ userCart,totalPrice });
 })
 
- userCart.products.push(newCartProduct._id)
- await userCart.save()
-}
-
-
-res.redirect('back')
-
-})
-
-router.get('/profile', (req, res, next) => {
-  res.render('profile')
+router.get('/remove/:cartProductId',isLoggedIn,async function (req, res) {
+  await cartProductModel.findOneAndDelete({_id:req.params.cartProductId})
+  res.redirect('back')
 })
 
 
+
+router.get('/AddToCart/:productId',isLoggedIn,async function (req, res) {
+  const productId=req.params.productId
+  let userCart=await cartModel.findOne({
+    user:req.user._id,
+  })
+  if(!userCart){
+    let userCart=await cartModel.create({
+      user:req.user._id
+    })
+  }
+
+  let newcartproduct=await cartProductModel.findOne({
+    product:productId,
+    _id:{$in:userCart.product}
+  })
+  if(newcartproduct){
+    newcartproduct.quantity=newcartproduct.quantity+1
+    await newcartproduct.save()
+  }
+  else{
+    newcartproduct=await cartProductModel.create({
+      product:productId,
+      quantity:1,
+    })
+    userCart.product.push(newcartproduct._id)
+    await userCart.save()
+  }
+  res.redirect('back')
+})
+
+router.post('/updateQuantity',isLoggedIn,async function (req, res) {
+  await cartProductModel.findOneAndUpdate({_id:req.body.cartProductId},{quantity:req.body.quantity})
+  res.json({message:"quantity updated"})
+})
 
 module.exports = router;
